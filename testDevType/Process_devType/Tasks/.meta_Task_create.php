b12d556bcd873c02d877266753742e24316f7509
qa4ubiqube <>
2021-04-14 07:36:44 +0000

    Updating BPMN on  - 14-04-2021 07:36:44 - ncroot

--- testDevType/Process_devType/Tasks/.meta_Task_create.php
+++ testDevType/Process_devType/Tasks/.meta_Task_create.php
@@ -7,7 +7,7 @@
         </entry>
         <entry>
             <key>DATE_MODIFICATION</key>
-            <value>1618385775002</value>
+            <value>1618385804937</value>
         </entry>
         <entry>
             <key>REPOSITORY</key>
@@ -15,7 +15,7 @@
         </entry>
         <entry>
             <key>DATE_CREATION</key>
-            <value>1618385774998</value>
+            <value>1618385804934</value>
         </entry>
         <entry>
             <key>TAG</key>
