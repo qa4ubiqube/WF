f91bf9b14065b2cff5e1c63585a9ebbe15cea530
qa4ubiqube <>
2021-04-14 08:16:19 +0000

    Updating BPMN on  - 14-04-2021 08:16:19 - ncroot

--- WF_METype/Process_P1/Tasks/.meta_Task_T1.py
+++ WF_METype/Process_P1/Tasks/.meta_Task_T1.py
@@ -7,7 +7,7 @@
         </entry>
         <entry>
             <key>DATE_MODIFICATION</key>
-            <value>1618388167306</value>
+            <value>1618388179576</value>
         </entry>
         <entry>
             <key>REPOSITORY</key>
@@ -15,7 +15,7 @@
         </entry>
         <entry>
             <key>DATE_CREATION</key>
-            <value>1618388167302</value>
+            <value>1618388179573</value>
         </entry>
         <entry>
             <key>TAG</key>
