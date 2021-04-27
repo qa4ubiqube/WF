907ebf56b91324e5d6eb783e91fd97f5e4ca1444
jboss <jboss@ec5e354cab26>
2021-04-27 13:11:26 +0000

    Updating Workflow Task on  - 27-04-2021 13:11:26 - ncroot

--- www/Process_ppp/Tasks/.meta_redfish_inventory_fw_CREATE.php
+++ www/Process_ppp/Tasks/.meta_redfish_inventory_fw_CREATE.php
@@ -7,7 +7,7 @@
         </entry>
         <entry>
             <key>DATE_MODIFICATION</key>
-            <value>1619528644555</value>
+            <value>1619529086465</value>
         </entry>
         <entry>
             <key>REPOSITORY</key>
@@ -15,7 +15,7 @@
         </entry>
         <entry>
             <key>DATE_CREATION</key>
-            <value>1619528644555</value>
+            <value>1619529086465</value>
         </entry>
         <entry>
             <key>TAG</key>
