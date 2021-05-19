eaf90b4a8a2886a2b3122c6c56ee9bc05f5e34ba
hiro-ubiqube <>
2021-05-19 23:42:53 +0000

    Updating Workflow on  - 19-05-2021 23:42:53 - ncroot

--- Send_Log/Process_Send_log/Tasks/.meta_Task_send_log.py
+++ Send_Log/Process_Send_log/Tasks/.meta_Task_send_log.py
@@ -7,7 +7,7 @@
         </entry>
         <entry>
             <key>DATE_MODIFICATION</key>
-            <value>1621467741901</value>
+            <value>1621467773638</value>
         </entry>
         <entry>
             <key>REPOSITORY</key>
@@ -15,7 +15,7 @@
         </entry>
         <entry>
             <key>DATE_CREATION</key>
-            <value>1621467741868</value>
+            <value>1621467773634</value>
         </entry>
         <entry>
             <key>TAG</key>
--- Send_Log/Process_Send_log/Tasks/Task_send_log.py
+++ Send_Log/Process_Send_log/Tasks/Task_send_log.py
@@ -31,5 +31,5 @@ headers = {'content-type': 'application/json'}
 
 r = requests.post(url, json=payload, headers=headers)
 
-ret = MSA_API.process_content('ENDED', url+payload, context, True)
+ret = MSA_API.process_content('ENDED', url, context, True)
 print(ret)
