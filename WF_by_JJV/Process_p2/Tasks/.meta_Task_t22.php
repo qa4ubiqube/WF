afdc713ee4e88da89b9bf1f88c479f637f64a451
ncroot <>
2021-09-08 17:09:06 +0000

    Updating Workflow on  - 08-09-2021 17:09:06 - ncroot

--- WF_by_JJV/Process_p2/Tasks/.meta_Task_t22.php
+++ WF_by_JJV/Process_p2/Tasks/.meta_Task_t22.php
@@ -7,7 +7,7 @@
         </entry>
         <entry>
             <key>DATE_MODIFICATION</key>
-            <value>1631120883485</value>
+            <value>1631120946157</value>
         </entry>
         <entry>
             <key>REPOSITORY</key>
@@ -15,7 +15,7 @@
         </entry>
         <entry>
             <key>DATE_CREATION</key>
-            <value>1631120883481</value>
+            <value>1631120946152</value>
         </entry>
         <entry>
             <key>TAG</key>
--- WF_by_JJV/Process_p2/Tasks/Task_t22.php
+++ WF_by_JJV/Process_p2/Tasks/Task_t22.php
@@ -5,9 +5,7 @@ require_once '/opt/fmc_repository/Process/Reference/Common/common.php';
 function list_args()
 {
   create_var_def('sleep', 'Integer');
-  create_var_def('mydevice', 'Device');
-  create_var_def('int', 'Integer');
-  create_var_def('ref', 'OBMFRef');
+
   
 }
 
