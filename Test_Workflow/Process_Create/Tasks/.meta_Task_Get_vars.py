db58afda470cbc72fbfbe297afd506e7661fe6c4
ncroot <>
2021-09-29 13:20:15 +0000

    Updating Workflow on  - 29-09-2021 13:20:15 - ncroot

--- Test_Workflow/Process_Create/Tasks/.meta_Task_Get_vars.py
+++ Test_Workflow/Process_Create/Tasks/.meta_Task_Get_vars.py
@@ -7,7 +7,7 @@
         </entry>
         <entry>
             <key>DATE_MODIFICATION</key>
-            <value>1632921603195</value>
+            <value>1632921615718</value>
         </entry>
         <entry>
             <key>REPOSITORY</key>
@@ -15,7 +15,7 @@
         </entry>
         <entry>
             <key>DATE_CREATION</key>
-            <value>1632921603193</value>
+            <value>1632921615714</value>
         </entry>
         <entry>
             <key>TAG</key>
--- Test_Workflow/Process_Create/Tasks/Task_Get_vars.py
+++ Test_Workflow/Process_Create/Tasks/Task_Get_vars.py
@@ -5,21 +5,6 @@ from msa_sdk.variables import Variables
 from msa_sdk.msa_api import MSA_API
 
 '''
-List all the parameters required by the task
-
-You can use var_name convention for your variables
-They will display automaticaly as "Var Name"
-The allowed types are:
-  'String', 'Boolean', 'Integer', 'Password', 'IpAddress',
-  'IpMask', 'Ipv6Address', 'Composite', 'OBMFRef', 'Device'
-
- Add as many variables as needed
-'''
-dev_var = Variables()
-dev_var.add('var_name', var_type='String')
-dev_var.add('var_name2', var_type='Integer')
-
-'''
 context => Service Context variable per Service Instance
 All the user-inputs of Tasks are automatically stored in context
 Also, any new variables should be stored in context which are used across Service Instance
