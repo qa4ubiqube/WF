44fc0ea8b006965b5129d2a12fd5d2a96fba25a4
ncroot <>
2021-09-29 15:06:29 +0000

    Updating Workflow on  - 29-09-2021 15:06:29 - ncroot

--- Test_Workflow/Process_Create/Tasks/.meta_Task_Get_vars.py
+++ Test_Workflow/Process_Create/Tasks/.meta_Task_Get_vars.py
@@ -7,7 +7,7 @@
         </entry>
         <entry>
             <key>DATE_MODIFICATION</key>
-            <value>1632927715177</value>
+            <value>1632927989141</value>
         </entry>
         <entry>
             <key>REPOSITORY</key>
@@ -15,7 +15,7 @@
         </entry>
         <entry>
             <key>DATE_CREATION</key>
-            <value>1632927715175</value>
+            <value>1632927989138</value>
         </entry>
         <entry>
             <key>TAG</key>
--- Test_Workflow/Process_Create/Tasks/Task_Get_vars.py
+++ Test_Workflow/Process_Create/Tasks/Task_Get_vars.py
@@ -3,16 +3,12 @@ Visit http://[YOUR_MSA_URL]/msa_sdk/ to see what you can import.
 '''
 from msa_sdk.variables import Variables
 from msa_sdk.msa_api import MSA_API
+from msa_sdk.util import util
 
 import requests
-headers = {
-            'Accept': 'application/json',
-            'Authorization': 'Bearer {}'.format(Variables.task_call()['TOKEN']),
-        }
-res = requests.get('http://localhost:8480/ubi-api-rest/system-admin/v1/msa_vars?name={}'.format('UBI_SMTP_IPADDR'), headers=headers)
 
-context = Variables.task_call()
+res = util.get_vars_value('UBI_SMTP_IPADDR')
 
-ret = MSA_API.process_content('ENDED', 'Task OK ' + res.json()[0]['value'], context, True)
+ret = MSA_API.process_content('ENDED', 'Task OK ' + res, context, True)
 print(ret)
 
