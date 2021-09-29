c963bf7033d5d9b6af1252743ba280d3790739b5
ncroot <>
2021-09-29 14:38:26 +0000

    Updating Workflow on  - 29-09-2021 14:38:26 - ncroot

--- Test_Workflow/Process_Create/Tasks/Task_Get_vars.py
+++ Test_Workflow/Process_Create/Tasks/Task_Get_vars.py
@@ -4,7 +4,7 @@ Visit http://[YOUR_MSA_URL]/msa_sdk/ to see what you can import.
 from msa_sdk.variables import Variables
 from msa_sdk.msa_api import MSA_API
 
-MSA_API._init_()
+MSA_API.__init__()
 MSA_API.path = 'system-admin/v1/msa_vars?name={}'.format('UBI_SMTP_IPADDR')
 res = MSA_API._call_get()
 
