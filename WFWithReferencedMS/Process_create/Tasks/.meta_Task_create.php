b58beb6f82fcf6182cad7852aef2099cbf4e4b4e
qa4ubiqube <>
2022-02-02 14:02:12 +0000

    Updating Workflow on  - 02-02-2022 14:02:12 - ncroot

--- WFWithReferencedMS/Process_create/Tasks/.meta_Task_create.php
+++ WFWithReferencedMS/Process_create/Tasks/.meta_Task_create.php
@@ -7,7 +7,7 @@
         </entry>
         <entry>
             <key>DATE_MODIFICATION</key>
-            <value>1643810520620</value>
+            <value>1643810532523</value>
         </entry>
         <entry>
             <key>REPOSITORY</key>
@@ -15,7 +15,7 @@
         </entry>
         <entry>
             <key>DATE_CREATION</key>
-            <value>1643810520579</value>
+            <value>1643810532504</value>
         </entry>
         <entry>
             <key>TAG</key>
--- WFWithReferencedMS/Process_create/Tasks/Task_create.php
+++ WFWithReferencedMS/Process_create/Tasks/Task_create.php
@@ -19,7 +19,7 @@ function list_args()
    *
    * Add as many variables as needed
    */
-  create_var_def('list', 'String');
+  create_var_def('list', 'Microservice Reference');
 
 }
 
