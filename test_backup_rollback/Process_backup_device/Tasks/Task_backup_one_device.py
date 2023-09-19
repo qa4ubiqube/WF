aee6368c01a9964e3a8e0eb9363a17d0396bbf6a
ncuser <ncuser@b5d2f96df646>
2023-09-19 19:16:03 +0000

    Initial commit with existing files

---
+++ test_backup_rollback/Process_backup_device/Tasks/Task_backup_one_device.py
@@ -0,0 +1,31 @@
+import json
+import time
+import ipaddress
+import os
+from msa_sdk.variables import Variables
+from msa_sdk import constants
+from msa_sdk import util
+from msa_sdk.msa_api import MSA_API
+
+dev_var = Variables()
+context = Variables.task_call(dev_var)
+ 
+device_name_IP            = context['device_name_IP']
+lock_file_name            = device_name_IP   
+lock_file_name            = context['lock_file_name']
+mode                      = 'w+'
+process_param             ={}
+process_param['process']           = 'test'
+sleep_time=30
+max_try_nb=10
+sleep_time=10
+max_try_nb=5
+
+
+#Obtain lock file.
+result = util.obtain_file_lock_exclusif(lock_file_name, process_param, mode, sleep_time, max_try_nb)
+result = json.loads(result)
+if result.get('wo_status') and result['wo_status'] != 'ENDED' and result.get('wo_comment'): 
+ MSA_API.task_error('OPERATION FAILED, File "'+lock_file_name+'" not locked : '+result['wo_comment'],context, True)
+ 
+MSA_API.task_success('OPERATION ENDED, File "'+lock_file_name+'" locked : '+result['wo_comment'],context, True)
