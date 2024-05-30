eafcc8b07503bd917b74d7954896a711ac4dbb31
ncuser <ncuser@ffc9b1e2d057>
2024-05-30 10:35:38 +0000

    Updating Workflow on  - 30-05-2024 10:35:38 - ncroot

--- test_backup_rollback/Process_backup_device/Task_backup_one_device.py
+++ test_backup_rollback/Process_backup_device/Task_backup_one_device.py
@@ -21,7 +21,7 @@ device_list = []
 device_list.append({"id": device_id})
 context['device_list'] = device_list
 
-result = list(backup_devices_configuration(device_list))
+result = backup_devices_configuration(device_list)
 # result={"date" : "14-09-2022 15:04:59", "message" : "SMS-CMD-FAILED","result" : "","revisionId" : -1,"status" : "FAIL"}
 #  result={"date" : "14-09-2022 15:08:00","message" : "BACKUP started","result" : "","revisionId" : -1,"status" : "RUNNING"
 
