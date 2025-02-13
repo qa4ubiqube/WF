8548b2b62253d32189c1a3520df1b7f95479a6c5
ncroot <>
2025-02-13 23:35:37 +0000

    Updating Workflow on  - 13-02-2025 23:35:37 - ncroot

---
+++ Jeffrey_Drake_RF/Process_Thomas_Jones_RF/Tasks/.meta_Task_name_Catherine_Boyd_RF.py
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<metadata>
+    <map>
+        <entry>
+            <key>TYPE</key>
+            <value>UPLOAD</value>
+        </entry>
+        <entry>
+            <key>REPOSITORY</key>
+            <value>Process</value>
+        </entry>
+        <entry>
+            <key>FILE_TYPE</key>
+            <value>text</value>
+        </entry>
+        <entry>
+            <key>DISPLAYNAME</key>
+            <value>Task_name_Catherine_Boyd_RF.py</value>
+        </entry>
+    </map>
+</metadata>
